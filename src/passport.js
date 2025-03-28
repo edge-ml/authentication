7425d46ce44c7be44fb465df0dd6a629be1edc91
Tobias King <tk.king.tk@gmail.com>
2025-03-28 10:34:34 +0100

    Updated dependencies

--- src/passport.js
+++ src/passport.js
@@ -1,104 +1,104 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const JwtStrategy = require('passport-jwt').Strategy;
-const { ExtractJwt } = require('passport-jwt');
-const bcrypt = require('bcrypt');
-const jwtDecode = require('jwt-decode');
-const GithubStrategy = require('passport-github2');
-const OAuth2Strategy = require('passport-oauth2');
-const Model = require('./models/userModel').model;
-const config = require('../config'); // Adjust the path to your config
-const { validateEmail } = require('./utils');
-const { generateToken } = require('./utils');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const { ExtractJwt } = require("passport-jwt");
+const bcrypt = require("bcrypt");
+const jwtDecode = require("jwt-decode");
+const GithubStrategy = require("passport-github2");
+const OAuth2Strategy = require("passport-oauth2");
+const Model = require("./models/userModel").model;
+const config = require("../config"); // Adjust the path to your config
+const { validateEmail } = require("./utils");
+const { generateToken } = require("./utils");
 
 // Local Strategy
 passport.use(
-	new LocalStrategy(
-		{ usernameField: 'email' },
-		async (email, password, done) => {
-			try {
-				let user;
-				if (validateEmail(email)) {
-					user = await Model.findOne({ email });
-				} else {
-					user = await Model.findOne({ userName: email });
-				}
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        let user;
+        if (validateEmail(email)) {
+          user = await Model.findOne({ email });
+        } else {
+          user = await Model.findOne({ userName: email });
+        }
 
-				if (!user) {
-					return done(null, false, { message: 'User not found' });
-				}
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
 
-				const isMatch = bcrypt.compareSync(password, user.password);
-				if (!isMatch) {
-					return done(null, false, { message: 'Incorrect password' });
-				}
-				return done(null, user);
-			} catch (error) {
-				return done(error);
-			}
-		}
-	)
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
 );
 
 passport.use(
-	new GithubStrategy(
-		{
-			clientID: config.GITHUB_CLIENT_ID,
-			clientSecret: config.GITHUB_CLIENT_SECRET,
-			callbackURL: 'http://localhost:3002/auth/login/callback',
-			scope: ['user:email'],
-			promt: 'select_account',
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			console.log(profile);
+  new GithubStrategy(
+    {
+      clientID: config.GITHUB_CLIENT_ID,
+      clientSecret: config.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3002/auth/login/callback",
+      scope: ["user:email"],
+      promt: "select_account",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
 
-			const user = {
-				email: profile.emails[0].value,
-				provider: 'github',
-				providerId: profile.id,
-				userName: profile.username,
-			};
-			console.log(user);
+      const user = {
+        email: profile.emails[0].value,
+        provider: "github",
+        providerId: profile.id,
+        userName: profile.username,
+      };
+      console.log(user);
 
-			const res = await Model.findOneAndUpdate(
-				{ provider: 'github', providerId: profile.id },
-				user,
-				{
-					upsert: true,
-					new: true,
-				}
-			);
-			console.log(res);
-			return done(null, res);
-		}
-	)
+      const res = await Model.findOneAndUpdate(
+        { provider: "github", providerId: profile.id },
+        user,
+        {
+          upsert: true,
+          new: true,
+        }
+      );
+      console.log(res);
+      return done(null, res);
+    }
+  )
 );
 
 const cookieExtractor = (req) => {
-	let jwt = null;
-	if (req && req.cookies) {
-		jwt = req.cookies.jwt;
-	}
-	console.log(jwt);
-	console.log(jwtDecode(jwt));
+  let jwt = null;
+  if (req && req.cookies) {
+    jwt = req.cookies.jwt;
+  }
+  console.log(jwt);
 
-	return jwt;
+  return jwt;
 };
 
 // JWT Strategy
 const opts = {
-	jwtFromRequest: cookieExtractor,
-	secretOrKey: config.SECRET_KEY,
+  jwtFromRequest: cookieExtractor,
+  secretOrKey: config.SECRET_KEY,
 };
 
 passport.use(
-	new JwtStrategy(opts, async (jwtPayload, done) => {
-		Model.findOne({ _id: jwtPayload.id }, (err, user) => {
-			if (err) return done(err, false);
-			if (user) return done(null, user);
-			return done(null, false);
-		});
-	})
+  new JwtStrategy(opts, async (jwtPayload, done) => {
+    try {
+      const user = await Model.findOne({ _id: jwtPayload.id });
+      return done(null, user || false);
+    } catch (err) {
+      return done(err, false);
+    }
+  })
 );
 
 module.exports = passport;
